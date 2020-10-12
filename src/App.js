24612d72e9932803c4d6e1033b8d81e962e759f2
Kenneth Carroll <kenneth.james.carroll@gmail.com>
2020-10-12 14:07:28 -0400

    Added responsive styles on About page for width 230px-812px

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React, {useState, useEffect} from 'react';
-import DesktopApp from './components/desktop/Desktop-App';
-import MobileApp from './components/mobile/Mobile-App';
+
+import About from './components/about/about';
+import Portfolio from './components/portfolio/portfolio';
+import Resume from './components/resume/resume';
+import Testimonial from './components/testimonial/testimonial';
+
+import Sidebar from './components/desktop/sidebar/sidebar';
+import NavBar from './components/mobile/navbar/navbar';
+import Footer from './components/mobile/footer/footer';
+
+import './App.css';
 
 function App() {
 
   const [isDesktop, toggleDesktop] = useState(true);
-
+  const [activeNav, setActiveNav] = useState('about');
   const updatePredicate = () => {
     toggleDesktop(window.innerWidth > 1024);
   }
@@ -16,7 +25,14 @@ function App() {
   }, [])
   
   return (
-    isDesktop ? <DesktopApp/> : <MobileApp/>
+    <div className="container">
+        {isDesktop ? <Sidebar activeNav={activeNav} setActiveNav={setActiveNav}/> : <NavBar setActiveNav={setActiveNav}/>}
+        { activeNav === 'about' ? <About setActiveNav={setActiveNav} isDesktop={isDesktop}/> : null}
+        { activeNav === 'portfolio' ? <Portfolio/> : null}
+        { activeNav === 'resume' ? <Resume /> : null}
+        { activeNav === 'testimonials' ? <Testimonial /> : null}
+        {isDesktop ? null : <Footer/>}
+		</div>
   );
 }
 
--- src/components/about/about.js
+++ src/components/about/about.js
@@ -13,20 +13,25 @@ import resumeDoc from "./assets/Resume.pdf";
 // End Assets Import
 
 const About = (props) => {
+
+    const image = (
+        <div className="about__upper__image__container">
+            <img className="about__upper__image" src={aboutImage} alt="Place Picture Here"/>
+        </div>);
     return(<div className="about">
     <div className="about__upper">
         <div className="about__upper__body">
             <strong><h1 className="about__upper__body__title">Kenneth Carroll</h1></strong>
+            {!props.isDesktop ? image : null}
             <h3 className="about__upper__body__subtitle">Full Stack Engineer</h3>
             <p className="about__upper__body__description">I'm an experienced engineer proficient across multiple technology stacks included web development, machine learning, and embedded systems. I am passionate about anything technology and am working on a Master's in CS alongside my current software projects.</p>
             <div className="about__upper__body__buttons">
-                <button className="about__upper__body__buttons__portfolio" onClick={()=>{props.setActiveNav('portfolio')}}><div className="about__upper__body__buttons__icon"><FontAwesomeIcon icon={faArrowCircleRight} size='1x'/></div>View Portfolio</button>
-                <a href={resumeDoc} target="_blank"className="about__upper__body__buttons__resume"><div className="about__upper__body__buttons__icon"><FontAwesomeIcon icon={faFileAlt} size='1x'/></div>Save Resume</a>
+                <button className="about__upper__body__buttons__portfolio" onClick={()=>{props.setActiveNav('portfolio')}}><div className="about__upper__body__buttons__icon"><FontAwesomeIcon icon={faArrowCircleRight} size='1x'/></div>Portfolio</button>
+                <a href={resumeDoc} target="_blank"className="about__upper__body__buttons__resume"><div className="about__upper__body__buttons__icon"><FontAwesomeIcon icon={faFileAlt} size='1x'/></div>Resume</a>
             </div>
+            {props.isDesktop ? image : null}
         </div>
-        <div className="about__upper__image__container">
-            <img className="about__upper__image" src={aboutImage} alt="Place Picture Here"/>
-        </div>
+        
     </div>
     <TileContainer/>
     
